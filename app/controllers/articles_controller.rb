39ba53ebbf0ee05ace833e39fad42f55af113b0d
Christophe Lafenetre <christophe.lafenetre@gmail.com>
2021-12-07 16:58:38 +0100

    index

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save!
+
+    redirect_to articles_path
+  end
+
+  def show
+    @article = 
+  end
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
